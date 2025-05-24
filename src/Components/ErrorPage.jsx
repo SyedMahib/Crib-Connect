2069122160fdcc64f3d6a554edcb1f05e05c1790
Syed Mahib <syedmahib8@gmail.com>
2025-05-24 23:49:15 +0600

    added error page

--- src/Components/ErrorPage.jsx
+++ src/Components/ErrorPage.jsx
@@ -14,11 +14,11 @@ const ErrorPage = () => {
         <div className='bg-white mt-50'>
             <div className='container mx-auto'>
                 <div>
-                    <img className='mx-auto w-7xlxl' src={Error} alt="Error404"/>
+                    <img className='mx-auto' src={Error} alt="Error404"/>
                 </div>
                 <div className='text-center mt-8'>
-                    <h1 className='text-4xl font-extrabold text-red-600'>Ooops! The page you are looking for doesn't exist</h1>
-                    <button onClick={handleGoHome} className='btn text-xl mt-10 py-6 bg-[#0EA106] text-white rounded-lg'>Go Back Home</button>
+                    <h1 className='text-xl md:text-4xl font-extrabold text-red-600'>Ooops! The page you are looking for doesn't exist</h1>
+                    <button onClick={handleGoHome} className='btn text-lg md:text-xl mt-10 py-6 bg-[#0EA106] text-white rounded-lg'>Go Back Home</button>
                 </div>
             </div>
         </div>
