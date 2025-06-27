2a209b7fe37b0878f1548502be1d136d7dc89c40
Syed Mahib <syedmahib8@gmail.com>
2025-06-27 18:26:20 +0600

    added error page

--- src/Components/ErrorPage.jsx
+++ src/Components/ErrorPage.jsx
@@ -1,28 +1,29 @@
-import React from 'react';
-import Error from "../assets/Error.jpg"
-import { useNavigate } from 'react-router';
+import React from "react";
+import Error from "../assets/errorLottie.json";
+import { Link } from "react-router";
+import Lottie from "lottie-react";
+import Navbar from "../Components/Navbar";
 
 const ErrorPage = () => {
-
-    const navigate = useNavigate();
-
-    const handleGoHome = () => {
-        navigate("/")
-    }
-
-    return (
-        <div className='bg-white mt-50'>
-            <div className='container mx-auto'>
-                <div>
-                    <img className='mx-auto' src={Error} alt="Error404"/>
-                </div>
-                <div className='text-center mt-8'>
-                    <h1 className='text-xl md:text-4xl font-extrabold text-red-600'>Ooops! The page you are looking for doesn't exist</h1>
-                    <button onClick={handleGoHome} className='btn text-lg md:text-xl mt-10 py-6 bg-[#0EA106] text-white rounded-lg'>Go Back Home</button>
-                </div>
-            </div>
+  return (
+    <div className="container mx-auto flex flex-col">
+      <Navbar></Navbar>
+      <div className="flex-grow container mx-auto flex items-center justify-center flex-col pt-[40px] pb-[87px]">
+        <div className="w-96 h-96 md:w-[500px] md:h-[500px] lg:w-[600px] lg:h-[600px] max-w-full max-h-[70vh]">
+          <Lottie animationData={Error} loop={true}></Lottie>
         </div>
-    );
+        <h2 className="text-center text-primary font-bold text-3xl">
+          Oops! You have landed on a wrong path
+        </h2>
+        <Link
+          to="/"
+          className="btn bg-[#F8B55F] text-primary font-bold mt-10"
+        >
+          Go back Home
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default ErrorPage;
